78265138961bcd7766f46968f380bb44fedbbe33
BogdanCrs <crusoveanubogdan4@gmail.com>
2020-10-09 16:01:17 +0300

    Updates

---
+++ precache-manifest.2535952c7cac255b1f9c48ccca238573.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd874ecd3b4096e7225149e21ea0788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c42f7085f2502e92d326",
+    "url": "/static/css/main.21fc5703.chunk.css"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/static/js/2.a9c43ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9c43ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42f7085f2502e92d326",
+    "url": "/static/js/main.547a1f35.chunk.js"
+  },
+  {
+    "revision": "0e95140b7bed8b651455",
+    "url": "/static/js/runtime-main.318786ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
